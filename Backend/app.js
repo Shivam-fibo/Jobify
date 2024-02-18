50275028e5c49d3478c054bdba565deecc9e1e3a
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2024-02-18 23:24:48 +0530

    router added

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
-
+import userRouter from "./routes/userRouter.js"
+import jobRouter from "./routes/jobRouter.js"
+import applicationRouter from "./routes/applicationRouter.js"
 const app = express();
 
 dotenv.config({ path : "./config/config.env"});
@@ -21,5 +23,9 @@ app.use(fileUpload({
     useTempFiles: true,
     tempFileDir : "/tmp/"
 }))
+
+app.use('/api/v1/user', userRouter)
+app.use('/api/v2/job', jobRouter)
+app.use('/api/v2/application', applicationRouter)
 
 export default app
