200350b48faf3085380a528cf47e4139e7b631f5
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2024-02-21 22:55:47 +0530

    code written for login and logout

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -24,4 +24,41 @@ export const register = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 201, res, "User Registered!");
   });
 
+export const login = catchAsyncErrors(async(req, res, next) =>{
+  const {email, password, role} = req.body
+  if( !email || !password || !role){
+    return next(
+      new ErrorHandler("Please provide email password and role" , 400)
+    );
+  }
+    const user = await User.findOne({ email }).select("+password");
+    if( !user){
+      return next(
+      new ErrorHandler("Invalid email or password" , 400)
+      )
+    }
+    const isPassword = await user.ccomparePassword(password);
+    if(!isPassword){
+      return next(
+      new ErrorHandler("Invalid email or password", 400)
+      )
+    }
+    if(user.role !== role){
+      return next(
+      new ErrorHandler("User with this role is not found",400)
+      )
+    }
+    sendToken(user, 200, res, "User logged in Succesfully!")
+
+});
+
+export const logout = catchAsyncErrors((req, res,next)=>{
+res.status(201).cookie("token", ""{
+  httpOnly : true,
+  expires: new Date(Date.now()),
+}).json({
+  sucess: true,
+  message: "User is succesfully log out"
+})
+})
   
