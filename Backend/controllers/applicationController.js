b21b990e7496b36f870d8eb0474095b4fa6c5045
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2024-02-25 21:57:25 +0530

    updated application controller

---
+++ Backend/controllers/applicationController.js
@@ -0,0 +1,6 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
+import ErrorHandler from "../middlewares/error.js";
+
+export const employerGetAllApplications = catchAsyncErrors(async(req,res,next) =>{
+    
+})
--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getAllJobs, getmyJobs, postJob, updateJob} from "../controllers/jobController.js"
+import {deleteJob, getAllJobs, getmyJobs, postJob, updateJob} from "../controllers/jobController.js"
 import {isAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router()
@@ -7,5 +7,6 @@ router.get("/getAll", getAllJobs)
 router.post("/post",isAuthenticated, postJob)
 router.get("/getmyJobs", getmyJobs)
 router.put("/update/:id", isAuthenticated,updateJob)
+router.delete("/delete/:id", isAuthenticated, deleteJob)
 
 export default router
