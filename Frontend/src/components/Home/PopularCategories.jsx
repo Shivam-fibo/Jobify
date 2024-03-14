4f35dea4893d8ad8efb2388040ddab77f531618a
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2024-03-14 22:52:37 +0530

    popular category

--- Frontend/src/components/Home/PopularCategories.jsx
+++ Frontend/src/components/Home/PopularCategories.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   MdOutlineDesignServices,
   MdOutlineWebhook,
@@ -9,6 +9,7 @@ import { TbAppsFilled } from "react-icons/tb";
 import { FaReact } from "react-icons/fa";
 import { GiArtificialIntelligence } from "react-icons/gi";
 import { IoGameController } from "react-icons/io5";
+
 const PopularCategories = () => {
   const categories = [
     {
@@ -61,14 +62,23 @@ const PopularCategories = () => {
     },
   ];
   return (
-    <div className='categories'>
+    <div className="categories">
       <h3>POPULAR CATEGORIES</h3>
-        <dir>
-
-        </dir>
-      
+      <div className="banner">
+        {categories.map((element) => {
+          return (
+            <div className="card" key={element.id}>
+              <div className="icon">{element.icon}</div>
+              <div className="text">
+                <p>{element.title}</p>
+                <p>{element.subTitle}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PopularCategories
+export default PopularCategories;
